0d09f77735527733640c98f9f473bf3b8d1398a2
caner <canerorenn@gmail.com>
2024-08-09 03:49:13 +0300

    created bookspider environment

---
+++ bookscraper/bookscraper/spiders/__init__.py
@@ -0,0 +1,21 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+
+# scrapy startproject bookscraper
+# scrapy genspider bookspider books.toscrape.com
+# scrapy shell
+# fetch('https://books.toscrape.com')
+# response.css('article.product_pod')
+# example: book.css('.product_price .product_color::text').get()
+# example: book.css('h3 a::text').get()
+# for link example:  book.css('h3 a').attrib['href']
+# scrapy crawl spider
+# for using mysql :'mysql --default-character-set=utf8mb4 -u root -p'
+#'mysql -h localhost -uroot -p'
+#'create database books;'
+#''use books;
+#'select * from books;'
+#'drop table books;
